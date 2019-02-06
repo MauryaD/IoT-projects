f8bdcd42a6652718ad258d5eb7c0d9bd24b64e13
DEEPAK MAURYA <deepakmaurya2198@gmail.com>
2019-02-06 20:27:10 +0530

    Add files via upload

---
+++ object_detection.js
@@ -0,0 +1,11 @@
+setChartLibrary('google-chart');
+
+setChartTitle('Distance of Objects');
+
+setChartType('lineGraph')
+;
+setAxisName('time','distance');
+
+setCrosshair(true);
+
+plotChart('time_stamp','receiver');
